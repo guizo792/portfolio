e8da50f4e7aee18e9c1f6cabb62b22026b68c621
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-10-19 23:48:36 +0100

    creating initial navigation bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.scss";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body-container">
+      <div className="nav-container">
+        <div className="nav-item logo">ABDELLAH GUIZOUL</div>
+        <ul className="nav-item nav-bar-container">
+          <li className="nav-bar-item">Home</li>
+          <li className="nav-bar-item">Projects</li>
+          <li className="nav-bar-item">Education</li>
+          <li className="nav-bar-item">Experience</li>
+          <li className="nav-bar-item">Blog</li>
+        </ul>
+        <div className="nav-item">
+          <button className="nav-btn">Contact Me</button>
+        </div>
+      </div>
+      <div className="main-container">
+        <section className="hero-section">
+          <div className="image-side">
+            <img src="" alt="" />
+          </div>
+          <div className="info-side"></div>
+        </section>
+      </div>
+      <div className="footer-container"></div>
     </div>
   );
 }
