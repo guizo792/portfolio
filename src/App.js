05e1fb82fffcf3e9c1d027b6f4ca15576637cbb7
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-10-20 03:15:10 +0100

    setting up initial version of hero section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="body-container">
       <div className="nav-container">
-        <div className="nav-item logo">ABDELLAH GUIZOUL</div>
+        <div className="nav-item logo">GUIZOUL</div>
         <ul className="nav-item nav-bar-container">
           <li className="nav-bar-item">Home</li>
           <li className="nav-bar-item">Projects</li>
@@ -18,9 +18,17 @@ function App() {
       <div className="main-container">
         <section className="hero-section">
           <div className="image-side">
-            <img src="" alt="" />
+            <img src={require("./template.png")} alt="that's me" />
           </div>
-          <div className="info-side"></div>
+          <div className="info-side">
+            <h1 className="info-side-title">Hello world!</h1>
+            <h2 className="info-side-title--secondary">
+              <span>I am </span> <span className="name">ABDELLAH</span> GUIZOUL
+            </h2>
+            <h1 className="info-side-title--tertiary ">
+              Enjoy your tour in my portfolio
+            </h1>
+          </div>
         </section>
       </div>
       <div className="footer-container"></div>
